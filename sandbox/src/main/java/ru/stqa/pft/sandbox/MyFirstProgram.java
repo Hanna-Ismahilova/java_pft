7c7ee090e80de280b3e68ea72d567d48619703cf
Hanna-Ismahilova <hannaismahilova@gmail.com>
2018-06-01 10:17:59 +0200

    change class name

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,11 @@
-public class MyFirstProgram{
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
     
     public static void main(String[] args){
   System.out.println("Hello, world!");
 }
 }
+
 
 
