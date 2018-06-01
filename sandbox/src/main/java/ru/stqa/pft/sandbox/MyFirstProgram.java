255abc9be5c15deb7d1ddf3bf49405e982f87e52
Hanna-Ismahilova <hannaismahilova@gmail.com>
2018-06-01 11:21:15 +0200

    Calculation code

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    
-    public static void main(String[] args){
-  System.out.println("Hello, world!");
-}
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    System.out.println(2+2);
+    System.out.println(2*2-5);
+    System.out.println(2-2+6);
+    System.out.println(2/2-88);
+  }
 }
 
 
